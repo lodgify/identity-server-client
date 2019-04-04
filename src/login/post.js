4aed134da7e2f1c0a661ab436462e67a3e2e1553
Jonny Gwillim <jonnygwillim@gmail.com>
2019-04-04 16:32:02 +0200

    feat(login): pass credentials header

--- src/login/post.js
+++ src/login/post.js
@@ -3,7 +3,7 @@ import { postJSON } from '@lodgify/isomorphic-fetch-helpers';
 import { ORIGIN } from './constants';
 import { getPathname } from './utils/getPathname';
 import { getUrl } from './utils/getUrl';
-import { PATHNAME_TEMPLATE } from './constants';
+import { INCLUDE_CREDENTIALS_HEADER, PATHNAME_TEMPLATE } from './constants';
 
 /**
  * @param  {number} websiteId
@@ -14,5 +14,5 @@ export const post = (websiteId, formValues) => {
   const pathname = getPathname(PATHNAME_TEMPLATE, websiteId);
   const url = getUrl(ORIGIN, pathname);
 
-  return postJSON(url, formValues);
+  return postJSON(url, formValues, INCLUDE_CREDENTIALS_HEADER);
 };
